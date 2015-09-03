ee15af5d2b6c89b9afd3ef86320639a81cc816f8
Paul Melis <paul@floorball-flamingos.nl>
2015-09-03 21:30:11 +0200

    - Corrected reference count of objects returned from   readply(). They were one too high (O instead of N   in Py_BuildValue). - Use shadow object to handle array deallocation. Based   on an Enthought blog post by Travis Olivant. - Editor stripped eol whitespace

--- readply.cpp
+++ readply.cpp
@@ -1,10 +1,10 @@
-/* 
+/*
 TODO:
 - need to handle faces with more than 4 vertices
-- vertex colors (red, green, blue)
-- normals
 - texture coordinates (s, t) OR (u,v)?
-- we assume property order is always correct, e.g. x,y,z. Need good way to handle this.
+- we assume property order in the file is always x,y,z. Need good way to handle other orders
+- add parameter to specify if returned vertex color array is
+  blender-style (color per vertex per face) or plain per-vertex
 */
 
 #define NPY_NO_DEPRECATED_API NPY_1_9_API_VERSION
@@ -15,10 +15,63 @@ TODO:
 #include <cstdio>
 #include <cassert>
 
+// After http://blog.enthought.com/general/numpy-arrays-with-pre-allocated-memory/
+typedef struct
+{
+    PyObject_HEAD
+    void *memory;
+}
+_MyDeallocObject;
+
+static void
+_mydealloc_dealloc(_MyDeallocObject *self)
+{
+    free(self->memory);
+#if PY_MAJOR_VERSION == 2
+    self->ob_type->tp_free((PyObject*)self);
+#elif PY_MAJOR_VERSION == 3
+    Py_TYPE(self)->tp_free((PyObject*)self);
+#endif
+}
+
+static PyTypeObject _MyDeallocType =
+{
+#if PY_MAJOR_VERSION == 2
+    PyObject_HEAD_INIT(NULL)
+    0,                          /*ob_size*/
+#elif PY_MAJOR_VERSION == 3
+    PyVarObject_HEAD_INIT(NULL, 0)
+#endif
+    "mydeallocator",            /*tp_name*/
+    sizeof(_MyDeallocObject),   /*tp_basicsize*/
+    0,                          /*tp_itemsize*/
+#if PY_MAJOR_VERSION == 2
+    _mydealloc_dealloc,         /*tp_dealloc*/
+#elif PY_MAJOR_VERSION == 3
+    (destructor)_mydealloc_dealloc,  /*tp_dealloc*/
+#endif
+    0,                          /*tp_print*/
+    0,                          /*tp_getattr*/
+    0,                          /*tp_setattr*/
+    0,                          /*tp_compare*/
+    0,                          /*tp_repr*/
+    0,                          /*tp_as_number*/
+    0,                          /*tp_as_sequence*/
+    0,                          /*tp_as_mapping*/
+    0,                          /*tp_hash */
+    0,                          /*tp_call*/
+    0,                          /*tp_str*/
+    0,                          /*tp_getattro*/
+    0,                          /*tp_setattro*/
+    0,                          /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,             /*tp_flags*/
+    "Internal deallocator object",  /* tp_doc */
+};
+
 static float    *vertices = NULL;
 static int      next_vertex_element_offset = 0;
 
-static int      *faces = NULL;                      
+static int      *faces = NULL;
 static int      next_face_element_offset = 0;
 static int      num_triangles, num_quads;
 
@@ -29,8 +82,8 @@ static float    *vertex_colors = NULL;
 static int      next_vertex_color_element_offset = 0;
 static float    vertex_color_scale_factor = 1.0;
 
-static int 
-vertex_cb(p_ply_argument argument) 
+static int
+vertex_cb(p_ply_argument argument)
 {
     vertices[next_vertex_element_offset] = ply_get_argument_value(argument);
     next_vertex_element_offset++;
@@ -38,8 +91,8 @@ vertex_cb(p_ply_argument argument)
     return 1;
 }
 
-static int 
-vertex_color_cb(p_ply_argument argument) 
+static int
+vertex_color_cb(p_ply_argument argument)
 {
     vertex_colors[next_vertex_color_element_offset] = ply_get_argument_value(argument) * vertex_color_scale_factor;
     next_vertex_color_element_offset++;
@@ -47,8 +100,8 @@ vertex_color_cb(p_ply_argument argument)
     return 1;
 }
 
-static int 
-vertex_normal_cb(p_ply_argument argument) 
+static int
+vertex_normal_cb(p_ply_argument argument)
 {
     vertex_normals[next_vertex_normal_element_offset] = ply_get_argument_value(argument);
     next_vertex_normal_element_offset++;
@@ -56,14 +109,14 @@ vertex_normal_cb(p_ply_argument argument)
     return 1;
 }
 
-static int 
-face_cb(p_ply_argument argument) 
+static int
+face_cb(p_ply_argument argument)
 {
     long    length, value_index;
     int     vertex_index;
-    
+
     ply_get_argument_property(argument, NULL, &length, &value_index);
-    
+
     if (value_index == -1)
     {
         if (length > 4)
@@ -72,23 +125,23 @@ face_cb(p_ply_argument argument)
             num_triangles++;
         else if (length == 4)
             num_quads++;
-        
+
         return 1;
     }
-    
-    vertex_index = ply_get_argument_value(argument);    
+
+    vertex_index = ply_get_argument_value(argument);
     faces[next_face_element_offset] = vertex_index;
     next_face_element_offset++;
-    
+
     // Blender's vertices_raw array uses 4 vertex indices per face,
-    // for denoting either a triangle or a quad. 
-    // For a triangle the last (fourth) index needs to be 0. 
-        
+    // for denoting either a triangle or a quad.
+    // For a triangle the last (fourth) index needs to be 0.
+
     if (length == 3 && value_index == 2)
     {
         // Last index of triangle was just added to the index array.
         // Add extra 0 index to get to 4 indices per face
-        faces[next_face_element_offset] = 0;        
+        faces[next_face_element_offset] = 0;
         next_face_element_offset++;
     }
     else if (length == 4 && value_index == 3 && vertex_index == 0)
@@ -97,7 +150,7 @@ face_cb(p_ply_argument argument)
         // vertex colors, normals, texcoords. Simply flag the quad as
         // needing reordering.
         // Handle the case when there is a quad that has indices i, j, k, 0.
-        // We should cycle the indices to move the 0 out of the last place, 
+        // We should cycle the indices to move the 0 out of the last place,
         // as it would otherwise get interpreted as a triangle.
         const int firstidx = next_face_element_offset-4;
         faces[firstidx+3] = faces[firstidx+2];
@@ -105,92 +158,92 @@ face_cb(p_ply_argument argument)
         faces[firstidx+1] = faces[firstidx];
         faces[firstidx] = 0;
     }
-        
+
     return 1;
 }
 
-static PyObject* 
+static PyObject*
 readply(PyObject* self, PyObject* args)
 {
-    const char *fname;  
-    
+    const char *fname;
+
     if (!PyArg_ParseTuple(args, "s", &fname))
         return NULL;
-        
+
     p_ply ply = ply_open(fname, NULL, 0, NULL);
-    if (!ply)     
+    if (!ply)
     {
         PyErr_SetString(PyExc_IOError, "Could not open PLY file");
-        return NULL;        
+        return NULL;
     }
-    
-    if (!ply_read_header(ply)) 
+
+    if (!ply_read_header(ply))
     {
         PyErr_SetString(PyExc_IOError, "Could not read PLY header");
         ply_close(ply);
-        return NULL;            
-    }    
-    
+        return NULL;
+    }
+
     // Check elements
-    
-    p_ply_element   vertex_element=NULL, face_element=NULL;    
+
+    p_ply_element   vertex_element=NULL, face_element=NULL;
     const char      *name;
-    
+
     p_ply_element element = ply_get_next_element(ply, NULL);
     while (element)
     {
         ply_get_element_info(element, &name, NULL);
-        
+
         if (strcmp(name, "vertex") == 0)
             vertex_element = element;
         else if (strcmp(name, "face") == 0)
             face_element = element;
-        
+
         element = ply_get_next_element(ply, element);
     }
-    
+
     assert(vertex_element && "Don't have a vertex element");
     assert(face_element && "Don't have a face element");
-    
+
     // Set vertex and face callbacks
-    
+
     long nvertices, nfaces;
-    
+
     nvertices = ply_set_read_cb(ply, "vertex", "x", vertex_cb, NULL, 0);
     ply_set_read_cb(ply, "vertex", "y", vertex_cb, NULL, 0);
     ply_set_read_cb(ply, "vertex", "z", vertex_cb, NULL, 1);
-    
+
     nfaces = ply_set_read_cb(ply, "face", "vertex_indices", face_cb, NULL, 0);
-    
+
     printf("%ld vertices\n%ld faces\n", nvertices, nfaces);
-    
+
     // Set optional callbacks
-    
+
     bool            have_vertex_colors = false;
     bool            have_vertex_normals = false;
-    
+
     p_ply_property  prop;
     e_ply_type      ptype, plength_type, pvalue_type;
-        
+
     prop = ply_get_next_property(vertex_element, NULL);
     while (prop)
     {
         ply_get_property_info(prop, &name, &ptype, &plength_type, &pvalue_type);
-        
+
         printf("%s\n", name);
-        
+
         if (strcmp(name, "red") == 0)
         {
             have_vertex_colors = true;
-            
+
             if (ptype == PLY_UCHAR)
                 vertex_color_scale_factor = 1.0 / 255;
             else if (ptype != PLY_FLOAT)
                 printf("Warning: vertex color value type is %d, don't know how to handle!\n", ptype);
-            
+
             ply_set_read_cb(ply, "vertex", "red", vertex_color_cb, NULL, 0);
             ply_set_read_cb(ply, "vertex", "green", vertex_color_cb, NULL, 0);
-            ply_set_read_cb(ply, "vertex", "blue", vertex_color_cb, NULL, 1);            
+            ply_set_read_cb(ply, "vertex", "blue", vertex_color_cb, NULL, 1);
         }
         else if (strcmp(name, "nx") == 0)
         {
@@ -198,40 +251,40 @@ readply(PyObject* self, PyObject* args)
 
             ply_set_read_cb(ply, "vertex", "nx", vertex_normal_cb, NULL, 0);
             ply_set_read_cb(ply, "vertex", "ny", vertex_normal_cb, NULL, 0);
-            ply_set_read_cb(ply, "vertex", "nz", vertex_normal_cb, NULL, 1);            
+            ply_set_read_cb(ply, "vertex", "nz", vertex_normal_cb, NULL, 1);
         }
-        
+
         prop = ply_get_next_property(vertex_element, prop);
     }
-    
-    // Allocate memory, initialize    
-    
+
+    // Allocate memory, initialize
+
     vertices = (float*) malloc(sizeof(float)*nvertices*3);
-    next_vertex_element_offset = 0;    
-        
+    next_vertex_element_offset = 0;
+
     faces = (int*) malloc(sizeof(int)*nfaces*4);
     next_face_element_offset = 0;
-    
+
     if (have_vertex_normals)
     {
         vertex_normals = (float*) malloc(sizeof(float)*nvertices*3);
-        next_vertex_normal_element_offset = 0;    
-    }    
+        next_vertex_normal_element_offset = 0;
+    }
 
     if (have_vertex_colors)
     {
         vertex_colors = (float*) malloc(sizeof(float)*nvertices*3);
-        next_vertex_color_element_offset = 0;    
+        next_vertex_color_element_offset = 0;
     }
-    
+
     // Let rply process the file using the callbacks
-    
+
     num_triangles = num_quads = 0;
-        
-    if (!ply_read(ply)) 
+
+    if (!ply_read(ply))
     {
         PyErr_SetString(PyExc_IOError, "Could not read PLY data");
-        
+
         ply_close(ply);
         free(vertices);
         free(faces);
@@ -239,90 +292,101 @@ readply(PyObject* self, PyObject* args)
             free(vertex_normals);
         if (have_vertex_colors)
             free(vertex_colors);
-        
-        return NULL;            
+
+        return NULL;
     }
-    
+
     printf("%d triangles, %d quads\n", num_triangles, num_quads);
-    
+
     // Clean up
-    
+
     ply_close(ply);
-    
+
     // Return stuff
-    
+
     npy_intp    np_vertices_dims[1] = { nvertices*3 };
     npy_intp    np_faces_dims[1] = { nfaces*4 };
-    
+
+    PyObject *newobj;
+
+    // XXX check for NULL in return of PyArray_SimpleNewFromData() and PyObject_New()
     // http://stackoverflow.com/questions/27912483/memory-leak-in-python-extension-when-array-is-created-with-pyarray-simplenewfrom
-    PyArrayObject *np_vertices = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_vertices_dims, NPY_FLOAT, vertices);  
-    PyArray_ENABLEFLAGS(np_vertices, NPY_ARRAY_OWNDATA);
-    
-    PyArrayObject *np_faces = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_faces_dims, NPY_INT, faces);  
-    PyArray_ENABLEFLAGS(np_faces, NPY_ARRAY_OWNDATA);
-    
+    PyArrayObject *np_vertices = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_vertices_dims, NPY_FLOAT, vertices);
+    newobj = (PyObject*)PyObject_New(_MyDeallocObject, &_MyDeallocType);
+    ((_MyDeallocObject *)newobj)->memory = vertices;
+    PyArray_SetBaseObject(np_vertices, newobj);
+
+    PyArrayObject *np_faces = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_faces_dims, NPY_INT, faces);
+    newobj = (PyObject*)PyObject_New(_MyDeallocObject, &_MyDeallocType);
+    ((_MyDeallocObject *)newobj)->memory = faces;
+    PyArray_SetBaseObject(np_faces, newobj);
+
     PyObject *np_vcolors, *np_vnormals;
-    
+
     if (have_vertex_normals)
     {
-        PyArrayObject *arr = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_vertices_dims, NPY_FLOAT, vertex_normals);  
-        PyArray_ENABLEFLAGS(arr, NPY_ARRAY_OWNDATA);
-        
+        PyArrayObject *arr = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_vertices_dims, NPY_FLOAT, vertex_normals);
+        newobj = (PyObject*)PyObject_New(_MyDeallocObject, &_MyDeallocType);
+        ((_MyDeallocObject *)newobj)->memory = vertex_normals;
+        PyArray_SetBaseObject(arr, newobj);
+
         np_vnormals = (PyObject*) arr;
     }
     else
     {
         np_vnormals = Py_None;
         Py_XINCREF(np_vnormals);
-    }    
-    
+    }
+
     if (have_vertex_colors)
     {
         /* Direct return of per-vertex colors
-        PyArrayObject *arr = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_vertices_dims, NPY_FLOAT, vertex_colors);  
+        PyArrayObject *arr = (PyArrayObject*) PyArray_SimpleNewFromData(1, np_vertices_dims, NPY_FLOAT, vertex_colors);
         */
-        
+
         // Colors per vertex
         // ->
         // Colors per vertex per face
-        
+
         const int n = 3*((num_triangles*3)+(num_quads*4));
-        
+
         float   *vcol2 = (float*) malloc (n*sizeof(float));
         float   *vcol2color = vcol2;
         float   *col;
         int     vi;
-        
+
         for (int fi = 0; fi < nfaces; fi++)
         {
             const int *face = faces + 4*fi;
-            
+
             for (int i = 0; i < 4; i++)
             {
                 vi = face[i];
-                
+
                 if (i == 3 && vi == 0)
                 {
                     // Triangle
                     break;
                 }
-                
+
                 col = vertex_colors + 3*vi;
-                
+
                 vcol2color[0] = col[0];
                 vcol2color[1] = col[1];
-                vcol2color[2] = col[2];                
-                vcol2color += 3;            
+                vcol2color[2] = col[2];
+                vcol2color += 3;
             }
         }
-        
+
         free(vertex_colors);
-        
+
         npy_intp    dims[1] = { n };
-        PyArrayObject *arr = (PyArrayObject*) PyArray_SimpleNewFromData(1, dims, NPY_FLOAT, vcol2);  
-        
-        PyArray_ENABLEFLAGS(arr, NPY_ARRAY_OWNDATA);
-        
+        PyArrayObject *arr = (PyArrayObject*) PyArray_SimpleNewFromData(1, dims, NPY_FLOAT, vcol2);
+
+        newobj = (PyObject*)PyObject_New(_MyDeallocObject, &_MyDeallocType);
+        ((_MyDeallocObject *)newobj)->memory = vcol2;
+        PyArray_SetBaseObject(arr, newobj);
+
         np_vcolors = (PyObject*) arr;
     }
     else
@@ -330,8 +394,8 @@ readply(PyObject* self, PyObject* args)
         np_vcolors = Py_None;
         Py_XINCREF(np_vcolors);
     }
-    
-    return Py_BuildValue("(iiOOOO)",  nvertices, nfaces, np_vertices, np_faces, np_vnormals, np_vcolors);
+
+    return Py_BuildValue("(iiNNNN)",  nvertices, nfaces, np_vertices, np_faces, np_vnormals, np_vcolors);
 }
 
 static PyMethodDef ModuleMethods[] =
@@ -343,14 +407,14 @@ static PyMethodDef ModuleMethods[] =
 /* module initialization */
 
 #if PY_MAJOR_VERSION == 2
-PyMODINIT_FUNC 
+PyMODINIT_FUNC
 initmodule(void)
 {
-    (void) Py_InitModule("readply", ModuleMethods);     
+    (void) Py_InitModule("readply", ModuleMethods);
     import_array();
 }
 #elif PY_MAJOR_VERSION == 3
-static struct PyModuleDef module = 
+static struct PyModuleDef module =
 {
    PyModuleDef_HEAD_INIT,
    "readply",               /* name of module */
@@ -363,14 +427,18 @@ static struct PyModuleDef module =
 PyMODINIT_FUNC
 PyInit_readply(void)
 {
-PyObject *m;
+    PyObject *m;
+
+    _MyDeallocType.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&_MyDeallocType) < 0)
+        return NULL;
 
     m = PyModule_Create(&module);
     if (m == NULL)
         return NULL;
-        
+
     import_array();
-    
+
     return m;
 }
 #endif
