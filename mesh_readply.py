43b961915dd8428c0dc738b2aab83842a6e26c14
Paul Melis <paul.melis@surfsara.nl>
2016-09-20 17:00:43 +0200

    Make texcoords work in mesh_readply.py

--- mesh_readply.py
+++ mesh_readply.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # blender -P mesh_readply.py -- file.ply
 import sys, os, array, time, gc
-import bpy
+import bpy, bmesh
 
 # For uv coordinate handling, see http://blender.stackexchange.com/questions/4820/exporting-uv-coordinates
 
@@ -35,8 +35,6 @@ t0 = time.time()
 
 p = readply(fname)
 
-print(p.keys())
-
 t1 = time.time()
 print('PLY file read by readply() in %.3fs' % (t1-t0))
 
@@ -53,35 +51,38 @@ mesh.tessfaces.foreach_set('vertices_raw', p['faces'])
 mesh.validate()
 mesh.update()
 
-if 'vertex_colors' in p:
-    
-    """
-    # For each face, set the vertex colors of the vertices making up that face
-    for fi in range(num_faces):
-
-        # Get vertex indices for this triangle/quad
-        i, j, k, l = farray[4*fi:4*fi+4]
-
-        face_col = vcol_data[fi]
-        face_col.color1 = vcolarray[3*i:3*i+3]
-        face_col.color2 = vcolarray[3*j:3*j+3]
-        face_col.color3 = vcolarray[3*k:3*k+3]
-        if l != 0:
-            face_col.color4 = vcolarray[3*l:3*l+3]
-    """
-
+if 'vertex_colors' in p:   
     vcol_layer = mesh.vertex_colors.new()
     vcol_data = vcol_layer.data
     vcol_data.foreach_set('color', p['vertex_colors'])
+    
+mesh.validate()
+mesh.update()   
 
-if 'vertex_normals' in p:
+if 'vertex_normals' in p:    
     mesh.vertices.foreach_set('normal', p['vertex_normals'])
 
 if 'texcoords' in p:
-    print('Warning: vertex texcoords read from .ply file, but NOT applying vertex texture coordinates to blender object (yet)!')
+    
+    texcoords = p['texcoords']
+    texcoords = texcoords.reshape((texcoords.size//2, 2))
+    
+    mesh.uv_textures.new('default')
+    
+    bm = bmesh.new()
+    bm.from_mesh(mesh)
+    
+    uv_layer = bm.loops.layers.uv[0]
+    
+    for fi, f in enumerate(bm.faces):
+        for l in f.loops:
+            vi = l.vert.index
+            l[uv_layer].uv = tuple(texcoords[vi])
+    
+    bm.to_mesh(mesh)
 
 mesh.validate()
-mesh.update()
+mesh.update()   
 
 obj = bpy.data.objects.new('imported object', mesh)
 
