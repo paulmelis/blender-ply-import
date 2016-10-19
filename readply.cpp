9be9517cb15a9ba350e600f217377f5006d82f0c
Paul Melis <paul.melis@surfsara.nl>
2016-10-19 16:44:42 +0200

    Fix missing cstdint on VS2008

--- readply.cpp
+++ readply.cpp
@@ -19,6 +19,15 @@ TODO:
 #include <cstdio>
 #include <cassert>
 
+#ifdef _MSC_VER
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <cstdint>
+#endif
+
 //#define DEBUG
 
 //
