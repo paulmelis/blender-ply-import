0c53eb9500b34cad8b7a5dd5db6be15d8c048b71
Paul Melis <paul.melis@surfsara.nl>
2019-08-06 13:56:56 +0200

    Attempt to make setup.py runnable under Blender directly, to make sure the correct Python version is used to compile the module

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+try:
+    import bpy
+    # We're running in Blender, hack sys.argv to make setup() below
+    # do the right thing using Blender's version of Python
+    import sys
+    sys.argv = ['setup.py', 'build_ext', '--inplace']
+    
+except ImportError:
+    # Running outside blender, work as regular setup.py script
+    pass
+
 from setuptools import setup, Extension
 import numpy
 
